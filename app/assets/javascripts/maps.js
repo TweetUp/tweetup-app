4e9c91a9cd1a9308410b632fdc8eaf15a846d70c
Nikhila Ravi <nikkiravi92@gmail.com>
2015-05-23 15:33:58 +0200

    added maps

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,39 @@
+var map;
+
+      function initialize() {
+        var mapOptions = {
+          zoom: 10,
+          center: new google.maps.LatLng(53.563032, 9.930034)
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+        var markers = [{ lat: 53.550846, lng: 9.930549}, {lat: 53.575519, lng: 10.008655}];
+
+        placeMarkers(markers);
+      }
+
+      function loadScript() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '?key=AIzaSyB1ZZktWMg-Aiihtzmno8SNV_HPKj1yCHY' +
+            '&signed_in=true&callback=initialize';
+        document.body.appendChild(script);
+      }
+
+      function addMarker(coordinates) {
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(coordinates.lat, coordinates.lng),
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: 'Hello World!'
+        });
+      }
+
+      function placeMarkers(markers) {
+        for (var i=0; i<markers.length; i++) {
+          addMarker(markers[i]);
+        }
+      }
+     
+      window.onload = loadScript;
