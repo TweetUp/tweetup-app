2dc23349715a1ffd58b1e9157fb85e735ef92efa
Julika Bödewadt <julika.boedewadt@xing.com>
2015-05-23 17:05:43 +0200

    improved js (namespacing)

--- app/assets/javascripts/maps/maps.js
+++ app/assets/javascripts/maps/maps.js
@@ -1,39 +1,40 @@
-var map;
+var tweetup  = {
 
-function initialize() {
-  var mapOptions = {
-    zoom: 10,
-    center: new google.maps.LatLng(53.563032, 9.930034)
-  };
+  initialize: function() {
+    // general map options
+    var options = {
+          zoom: 10,
+          center: new google.maps.LatLng(53.563032, 9.930034)
+        },
+        markers = [{ lat: 53.550846, lng: 9.930549}, {lat: 53.575519, lng: 10.008655}];
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-  var markers = [{ lat: 53.550846, lng: 9.930549}, {lat: 53.575519, lng: 10.008655}];
+    this.map = new google.maps.Map($("#map-canvas")[0], options);
+    this.placeMarkers(markers);
+  },
 
-  placeMarkers(markers);
-}
+  loadScript: function() {
+    var script = document.createElement('script');
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '?key=AIzaSyB1ZZktWMg-Aiihtzmno8SNV_HPKj1yCHY' +
-      '&signed_in=true&callback=initialize';
-  document.body.appendChild(script);
-}
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '?key=AIzaSyB1ZZktWMg-Aiihtzmno8SNV_HPKj1yCHY' +
+        '&signed_in=true&callback=tweetup.initialize';
+    document.body.appendChild(script);
+  },
 
-function addMarker(coordinates) {
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(coordinates.lat, coordinates.lng),
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Hello World!'
-  });
-}
+  addMarker: function(coordinates) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(coordinates.lat, coordinates.lng),
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      title: 'Hello World!'
+    });
+  },
 
-function placeMarkers(markers) {
-  for (var i=0; i<markers.length; i++) {
-    addMarker(markers[i]);
+  placeMarkers: function(markers) {
+    for (var i=0; i<markers.length; i++) {
+      this.addMarker(markers[i]);
+    }
   }
-}
+};
 
-window.onload = loadScript;
+window.onload = tweetup.loadScript();
